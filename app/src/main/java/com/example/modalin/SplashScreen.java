e9aa68ab3a4bfc275d3992938c89ef3dd54aa214
Ariq Naufal <ariiq48@gmail.com>
2019-02-22 22:33:43 +0700

    bismillah modal.in

---
+++ app/src/main/java/com/example/modalin/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.modalin;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        CountDownTimer timer = new CountDownTimer(3000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        timer.start();
+    }
+}
